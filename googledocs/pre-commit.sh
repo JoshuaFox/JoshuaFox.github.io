6e2b5d5d89ee2ec967fad2293ac661b1f61e9f85
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2021-12-17 16:20:02 +0200

    fix

--- googledocs/pre-commit.sh
+++ googledocs/pre-commit.sh
@@ -5,5 +5,5 @@ set -u
 python3 -m venv ./googledocs/venv
 source ./googledocs/venv/bin/activate
 pip3 install -r ./googledocs/requirements.txt
-python3 ./googledocs/export-yiddish-website-docs.py
-python3  ./googledocs/fix-googledocs-html.py
+#python3 ./googledocs/export-yiddish-website-docs.py
+#python3  ./googledocs/fix-googledocs-html.py
