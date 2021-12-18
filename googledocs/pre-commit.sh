eb46ed2de1cffdb4d301f2e9429cfc4f54528d84
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2021-12-18 18:25:40 +0200

    allow build on Git

--- googledocs/pre-commit.sh
+++ googledocs/pre-commit.sh
@@ -2,8 +2,8 @@
 set -e
 set -u
 
-python3 -m venv ./googledocs/venv
-source ./googledocs/venv/bin/activate
-pip3 install -r ./googledocs/requirements.txt
+#python3 -m venv ./googledocs/venv
+#source ./googledocs/venv/bin/activate
+#pip3 install -r ./googledocs/requirements.txt
 #python3 ./googledocs/export-yiddish-website-docs.py
 #python3  ./googledocs/fix-googledocs-html.py
