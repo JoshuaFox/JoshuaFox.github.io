74e4f0c5349f2086f3129356e588b6ba45d9ada9
Joshua T. Fox <joshua@doit-intl.com>
2022-08-15 20:22:44 +0300

    from Google Docs

---
+++ from_gdocs_to_site.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+./pre-process.sh
+git commit -am "from Google Docs"
+git push origin HEAD:master
--- preprocess.sh
+++ preprocess.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 ./googledocs/download-from-google-docs-and-process.sh
