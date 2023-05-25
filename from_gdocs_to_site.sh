4763ff181ed8cf54d68acaf009c149865bd86d84
Joshua Fox <joshua@doit-intl.com>
2023-05-25 18:10:12 +0300

    from Google Docs

--- from_gdocs_to_site.sh
+++ from_gdocs_to_site.sh
@@ -4,4 +4,3 @@ set -e
 ./googledocs/download-from-google-docs-and-process.sh
 git commit -am "from Google Docs"
 git push origin HEAD:master
-https://joshuafox.com/yiddish/װו%D6%BCהין%20קײנער%20איז%20פֿרי%D6%B4ער%20נישט%20געפֿאָרן/git
