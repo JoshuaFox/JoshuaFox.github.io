7effb90059f37ece8032accdc68536a66e9f8444
Joshua Fox <joshua@doit-intl.com>
2023-09-10 21:00:57 +0300

    from Google Docs

--- googledocs/export-yiddish-website-docs.py
+++ googledocs/export-yiddish-website-docs.py
@@ -125,7 +125,7 @@ if __name__ == "__main__":
         "*" * 30,
         '\nIf you see "Removed ...token.json" and then the  processing stops, you may need to kill and rerun,\n'
         'validating in the browser when asked.\n'
-        'and in particular, choosing "Continue" when it is given as the dispreferred option.',
+        'and in particular, choosing "Continue" when it is given as the dispreferred option.\n',
         "*" * 30,
         "\n",
         "*" * 30,
